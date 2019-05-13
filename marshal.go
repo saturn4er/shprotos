d68d2a52727c7bcc71e2ea505958f307349f7456
saturn4er <saturn4er@gmail.com>
2019-05-13 17:32:49 +0300

    Fix marshallerˆ

--- marshal.go
+++ marshal.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
+	"math"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/pkg/errors"
@@ -19,8 +20,10 @@ const (
 )
 
 func UnmarshalMessageBytesToMap(data []byte, msg *Message) (map[string]interface{}, error) {
+	return unmarshalMessageBytesToMap(proto.NewBuffer(data), msg)
+}
+func unmarshalMessageBytesToMap(buffer *proto.Buffer, msg *Message) (map[string]interface{}, error) {
 	result := make(map[string]interface{})
-	buffer := proto.NewBuffer(data)
 	for {
 		key, err := buffer.DecodeVarint()
 		if err != nil {
@@ -34,27 +37,33 @@ func UnmarshalMessageBytesToMap(data []byte, msg *Message) (map[string]interface
 		if !ok {
 			return nil, errors.Errorf("can't find field %d in message %s", fieldNum, msg.Name)
 		}
+		fmt.Println("parsing field ", messageField.GetName())
 		switch key & 7 {
 		case WireTypeVarint:
-			value, err := buffer.DecodeVarint()
-			if err != nil {
-				panic(err)
-			}
 			switch typ := messageField.GetType().(type) {
 			case *Enum:
+				value, err := buffer.DecodeVarint()
+				if err != nil {
+					return nil, errors.Wrap(err, "failed to decode varint")
+				}
 				result[messageField.GetName()] = value
 			case *Scalar:
 				if typ.ScalarName == "bytes" {
 					return nil, errors.New("can't assign varint to bytes field")
 				}
-				result[messageField.GetName()] = value
+
+				res, err := unmarshaScalar(buffer, typ)
+				if err != nil {
+					return nil, errors.Wrap(err, "failed to unmarshal varint scalar")
+				}
+				result[messageField.GetName()] = res
 			}
 		case WireType64Bit:
-			value, err := buffer.DecodeFixed64()
+			res, err := unmarshaScalar(buffer, messageField.GetType().(*Scalar))
 			if err != nil {
-				panic(err)
+				return nil, errors.Wrap(err, "failed to unmarshal varint scalar")
 			}
-			fmt.Printf("field %d is equal to %v\n", fieldNum, value)
+			result[messageField.GetName()] = res
 		case WireTypeLengthDelimited:
 			data, err := buffer.DecodeRawBytes(false)
 			if err != nil {
@@ -63,12 +72,75 @@ func UnmarshalMessageBytesToMap(data []byte, msg *Message) (map[string]interface
 
 			switch typ := messageField.GetType().(type) {
 			case *Map:
+				if _, ok := result[messageField.GetName()]; !ok {
+					result[messageField.GetName()] = make(map[interface{}]interface{})
+				}
+				resultMap := result[messageField.GetName()].(map[interface{}]interface{})
+
+				mapBuffer := proto.NewBuffer(data)
+				mapKeyKey, err := mapBuffer.DecodeVarint()
+				if err != nil {
+					return nil, errors.Wrap(err, "failed to decode map key key")
+				}
+				mapKeyWireType := mapKeyKey & 7
+
+				var mapKey interface{}
+				var mapValue interface{}
+				switch mapKeyWireType {
+				case WireTypeVarint, WireType64Bit, WireType32Bit:
+					res, err := unmarshaScalar(mapBuffer, typ.KeyType.(*Scalar))
+					if err != nil {
+						return nil, errors.Wrap(err, "failed to unmarshal varint scalar")
+					}
+					mapKey = res
+				case WireTypeLengthDelimited:
+					str, err := mapBuffer.DecodeStringBytes()
+					if err != nil {
+						return nil, errors.Wrap(err, "failed to decode string bytes")
+					}
+					mapKey = str
+				}
+
+				mapValueKey, err := mapBuffer.DecodeVarint()
+				if err != nil {
+					return nil, errors.Wrap(err, "failed to decode map value key")
+				}
+				mapValueWireType := mapValueKey & 7
+				switch mapValueWireType {
+				case WireTypeVarint, WireType32Bit, WireType64Bit:
+					res, err := unmarshaScalar(mapBuffer, typ.KeyType.(*Scalar))
+					if err != nil {
+						return nil, errors.Wrap(err, "failed to unmarshal varint scalar")
+					}
+					mapValue = res
+				case WireTypeLengthDelimited:
+					valueBytes, err := mapBuffer.DecodeRawBytes(false)
+					if err != nil {
+						return nil, errors.Wrap(err, "failed to decode map value raw bytes")
+					}
+					switch valueType := typ.ValueType.(type) {
+					case *Message:
+						msgValue, err := unmarshalMessageBytesToMap(proto.NewBuffer(valueBytes), valueType)
+						if err != nil {
+							return nil, errors.WithStack(err)
+						}
+						mapValue = msgValue
+					}
+				}
+				resultMap[mapKey] = mapValue
 			case *Message:
 				msgValue, err := UnmarshalMessageBytesToMap(data, typ)
 				if err != nil {
 					return nil, errors.WithStack(err)
 				}
-				result[messageField.GetName()] = msgValue
+				if messageField.IsRepeated() {
+					if _, ok := result[messageField.GetName()]; !ok {
+						result[messageField.GetName()] = []interface{}{}
+					}
+					result[messageField.GetName()] = append(result[messageField.GetName()].([]interface{}), msgValue)
+				} else {
+					result[messageField.GetName()] = msgValue
+				}
 			case *Scalar:
 				switch typ.ScalarName {
 				case "bytes":
@@ -86,7 +158,7 @@ func UnmarshalMessageBytesToMap(data []byte, msg *Message) (map[string]interface
 						buff := proto.NewBuffer(data)
 						var values []interface{}
 						for {
-							elem, err := buff.DecodeVarint()
+							elem, err := unmarshaScalar(buff, typ)
 							if err != nil {
 								if err == io.ErrUnexpectedEOF {
 									break
@@ -103,7 +175,7 @@ func UnmarshalMessageBytesToMap(data []byte, msg *Message) (map[string]interface
 
 			case *Enum:
 				buff := proto.NewBuffer(data)
-				var values []uint64
+				var values []int32
 				for {
 					elem, err := buff.DecodeVarint()
 					if err != nil {
@@ -112,7 +184,7 @@ func UnmarshalMessageBytesToMap(data []byte, msg *Message) (map[string]interface
 						}
 						return nil, errors.Wrap(err, "failed to get varint")
 					}
-					values = append(values, elem)
+					values = append(values, int32(elem))
 				}
 				result[messageField.GetName()] = values
 			default:
@@ -123,8 +195,98 @@ func UnmarshalMessageBytesToMap(data []byte, msg *Message) (map[string]interface
 		case WireTypeEndGroup:
 			fmt.Println("Unhandled group end")
 		case WireType32Bit:
-			fmt.Println("Unhandled 32 bit")
+			res, err := unmarshaScalar(buffer, messageField.GetType().(*Scalar))
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to unmarshal fixed 32 scalar")
+
+			}
+			result[messageField.GetName()] = res
 		}
 	}
 	return result, nil
+}
+
+func unmarshaScalar(buffer *proto.Buffer, scalar *Scalar) (interface{}, error) {
+	switch scalar.ScalarName {
+	case "sfixed32":
+		value, err := buffer.DecodeFixed32()
+		if err != nil {
+			if err == io.ErrUnexpectedEOF {
+				return nil, err
+			}
+			return nil, errors.Wrap(err, "failed to decode zigzag32")
+		}
+		return int32(value), nil
+	case "sfixed64":
+		value, err := buffer.DecodeFixed64()
+		if err != nil {
+			if err == io.ErrUnexpectedEOF {
+				return nil, err
+			}
+			return nil, errors.Wrap(err, "failed to decode zigzag64")
+		}
+		return int64(value), nil
+	case "sint32":
+		value, err := buffer.DecodeZigzag32()
+		if err != nil {
+			if err == io.ErrUnexpectedEOF {
+				return nil, err
+			}
+			return nil, errors.Wrap(err, "failed to decode zigzag32")
+		}
+		return int32(value), nil
+	case "sint64":
+		value, err := buffer.DecodeZigzag64()
+		if err != nil {
+			if err == io.ErrUnexpectedEOF {
+				return nil, err
+			}
+			return nil, errors.Wrap(err, "failed to decode zigzag64")
+		}
+		return int64(value), nil
+	case "fixed32":
+		value, err := buffer.DecodeFixed32()
+		if err != nil {
+			if err == io.ErrUnexpectedEOF {
+				return nil, err
+			}
+			return nil, errors.Wrap(err, "failed to decode fixed 32")
+		}
+		return uint32(value), nil
+	case "fixed64":
+		value, err := buffer.DecodeFixed64()
+		if err != nil {
+			if err == io.ErrUnexpectedEOF {
+				return nil, err
+			}
+			return nil, errors.Wrap(err, "failed to decode fixed 64")
+		}
+		return uint64(value), nil
+	case "int32", "int64", "uint32", "uint64", "bool", "double", "float":
+		value, err := buffer.DecodeVarint()
+		if err != nil {
+			if err == io.ErrUnexpectedEOF {
+				return nil, err
+			}
+			return nil, errors.Wrap(err, "failed to decode varint")
+		}
+		switch scalar.ScalarName {
+		case "int32":
+			return int32(value), nil
+		case "int64":
+			return int64(value), nil
+		case "uint32":
+			return uint32(value), nil
+		case "uint64":
+			return uint64(value), nil
+		case "bool":
+			return value == 1, nil
+		case "double":
+			return math.Float64frombits(value), nil
+		case "float":
+			return math.Float32frombits(uint32(value)), nil
+		}
+		return int32(value), nil
+	}
+	return nil, errors.Errorf("unknown scalar type: %s", scalar.ScalarName)
 }
--- marshal_test.go
+++ marshal_test.go
@@ -16,8 +16,19 @@ func TestUnmarshalMessageBytesToMap(t *testing.T) {
 	require.NoError(t, err)
 
 	protoMessage := &full.ComplexMessage{
-		Enum:   full.ComplexMessage_VALUE_A,
-		Scalar: 100,
+		Enum:           full.ComplexMessage_VALUE_A,
+		ScalarInt32:    -10,
+		ScalarInt64:    -20,
+		ScalarUint32:   10,
+		ScalarUint64:   20,
+		ScalarSint32:   -50,
+		ScalarSint64:   -500,
+		ScalarFixed32:  1000,
+		ScalarFixed64:  10000,
+		ScalarSfixed32: -100000,
+		ScalarSfixed64: -1000000,
+		ScalarBool:     false,
+		ScalarString:   "some another hello world",
 		Message: &full.ComplexMessage_SimpleMessage{
 			SomeField:  300,
 			SomeField2: "hello",
@@ -33,12 +44,12 @@ func TestUnmarshalMessageBytesToMap(t *testing.T) {
 			2: 3,
 			4: 5,
 		},
-		MapMsg: map[int32]*full.ComplexMessage_SimpleMessage{
-			1: {
+		MapMsg: map[string]*full.ComplexMessage_SimpleMessage{
+			"hello": {
 				SomeField:  1,
 				SomeField2: "hello",
 			},
-			2: {
+			"world": {
 				SomeField:  2,
 				SomeField2: "world",
 			},
@@ -71,23 +82,69 @@ func TestUnmarshalMessageBytesToMap(t *testing.T) {
 
 	res, err := UnmarshalMessageBytesToMap(data, msg)
 	require.NoError(t, err)
+
 	spew.Dump(res)
 
-	require.Equal(t, uint64(full.ComplexMessage_VALUE_A), res["enum"])
-	require.Equal(t, uint64(100), res["scalar"])
+	require.Equal(t, uint64(protoMessage.Enum), res["enum"])
+
+	require.Equal(t, protoMessage.ScalarInt32, res["scalar_int32"])
+	require.Equal(t, protoMessage.ScalarInt64, res["scalar_int64"])
+	require.Equal(t, protoMessage.ScalarUint32, res["scalar_uint32"])
+	require.Equal(t, protoMessage.ScalarUint64, res["scalar_uint64"])
+	require.Equal(t, protoMessage.ScalarSint32, res["scalar_sint32"])
+	require.Equal(t, protoMessage.ScalarSint64, res["scalar_sint64"])
+	require.Equal(t, protoMessage.ScalarFixed32, res["scalar_fixed32"])
+	require.Equal(t, protoMessage.ScalarFixed64, res["scalar_fixed64"])
+	require.Equal(t, protoMessage.ScalarSfixed32, res["scalar_sfixed32"])
+	require.Equal(t, protoMessage.ScalarSfixed64, res["scalar_sfixed64"])
+	require.Equal(t, protoMessage.ScalarBool, res["scalar_bool"])
+	require.Equal(t, protoMessage.ScalarString, res["scalar_string"])
 	require.Equal(t, map[string]interface{}{
-		"some_field":  uint64(300),
-		"some_field2": "hello",
+		"some_field":  protoMessage.Message.SomeField,
+		"some_field2": protoMessage.Message.SomeField2,
 	}, res["message"])
-	require.Equal(t, base64.StdEncoding.EncodeToString([]byte("hello world")), res["bytes"])
-	// require.Equal(t, res["map_enum"], )
-	// require.Equal(t, res["map_scalar"], )
-	// require.Equal(t, res["map_msg"], )
-	require.Equal(t, []interface{}{
-		uint64(full.ComplexMessage_VALUE_B7),
-		uint64(full.ComplexMessage_VALUE_B8),
+	require.Equal(t, base64.StdEncoding.EncodeToString(protoMessage.Bytes), res["bytes"])
+
+	require.Equal(t, map[interface{}]interface{}{
+		int32(1): int32(full.ComplexMessage_VALUE_A),
+		int32(2): int32(full.ComplexMessage_VALUE_B),
+		int32(3): int32(full.ComplexMessage_UNSPECIFIED),
+	}, res["map_enum"])
+	require.Equal(t, map[interface{}]interface{}{
+		int32(1): int32(2),
+		int32(2): int32(3),
+		int32(4): int32(5),
+	}, res["map_scalar"])
+	require.Equal(t, map[interface{}]interface{}{
+		"hello": map[string]interface{}{
+			"some_field":  int32(1),
+			"some_field2": "hello",
+		},
+		"world": map[string]interface{}{
+			"some_field":  int32(2),
+			"some_field2": "world",
+		},
+	}, res["map_msg"])
+
+	require.Equal(t, []int32{
+		int32(full.ComplexMessage_VALUE_B7),
+		int32(full.ComplexMessage_VALUE_B8),
 	}, res["r_enum"])
-	// require.Equal(t, res["r_scalar"], )
-	// require.Equal(t, res["r_msg"], )
-	// require.Equal(t, res["r_bytes"], )
+	require.Equal(t, []interface{}{
+		int32(1), int32(2), int32(3), int32(4),
+	}, res["r_scalar"])
+	require.Equal(t, []interface{}{
+		map[string]interface{}{
+			"some_field":  int32(1),
+			"some_field2": "hello",
+		},
+		map[string]interface{}{
+			"some_field":  int32(2),
+			"some_field2": "world",
+		},
+	}, res["r_msg"])
+	require.Equal(t, []interface{}{
+		"aGVsbG8=",
+		"d29ybGQ=",
+	}, res["r_bytes"])
 }
